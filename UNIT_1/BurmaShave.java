9558cdc8ae1ff5356f4f2a2fc0052ba5d017d1b4
Emanuel Ramos <115571450+Sledgehammer5568@users.noreply.github.com>
2023-11-17 16:16:22 -0600

    UNIT_1

---
+++ UNIT_1/BurmaShave.java
@@ -0,0 +1,45 @@
+/**
+ * Filename BurmaShave.java
+ *
+ * Coded by Emanuel Ramos
+ * 11/2/2023
+ *
+ * Programing Exercise 1-12
+ *
+ * // this is the original code provided
+ * class BurmaShave {
+ *     public static void main(String[] args) {
+ *         // Write your code here
+ *     }
+ * }
+ *
+ * Background:
+ * From 1925 through 1963, Burma Shave advertising signs
+ * appeared next to highways all across the United States.
+ * There were always four or five signs in a row containing
+ * pieces of a rhyme, followed by a final sign that read
+ * "Burma Shave." For example, one set of sign that have
+ * been preserved by the Smithsonian Institution reads as
+ * follows:
+ * Shaving brushes
+ * You'll soon see 'em
+ * On a shelf
+ * In some museum
+ * Burma Shave
+ *
+ * Instructions:
+ * Write, compile, and test a class that
+ * outputs five lines so that each
+ * displays one line of this Burma Shave
+ * slogan in turn.
+ */
+class BurmaShave {
+    public static void main(String[] args) {
+        // Write your code here
+        System.out.println("Shaving brushes");
+        System.out.println("You'll soon see 'em");
+        System.out.println("On a shelf");
+        System.out.println("In some museum");
+        System.out.println("Burma Shave");
+    }
+}
